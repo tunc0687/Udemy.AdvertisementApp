c1daa5946a713d03f444b4bc8a749e954876fb6f
tunc0687 <tunc0687@hotmail.com>
2024-03-10 23:27:54 +0300

    Add Configurations

--- Udemy.AdvertisementApp.Entities/AppUser.cs
+++ Udemy.AdvertisementApp.Entities/AppUser.cs
@@ -8,10 +8,12 @@ namespace Udemy.AdvertisementApp.Entities
 {
     public class AppUser : BaseEntity, IEntity
     {
-        public string UserName { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+        // public string Email { get; set; }
         public int GenderId { get; set; }
         public Gender Gender { get; set; }
 
