8a2688c4e1c7675d36935e352f91876b63e16c77
tunc0687 <tunc0687@hotmail.com>
2024-03-12 01:55:36 +0300

    Add FluentValidation

---
+++ Udemy.AdvertisementApp.Business/ValidationRules/ProvidedServiceCreateDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.AdvertisementApp.Dtos.ProvidedServiceDtos;
+
+namespace Udemy.AdvertisementApp.Business.ValidationRules
+{
+    public class ProvidedServiceCreateDtoValidator : AbstractValidator<ProvidedServiceCreateDto>
+    {
+        public ProvidedServiceCreateDtoValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.ImagePath).NotEmpty();
+        }
+    }
+}
